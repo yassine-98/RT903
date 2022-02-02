1f13d7bcab7cbbd36cd9709f9273e34c0349fa59
Yassine EL BAHI <76662289+yassine-98@users.noreply.github.com>
2022-02-02 10:13:53 +0100

    Update rt903.py

--- rt903.py
+++ rt903.py
@@ -4,13 +4,14 @@ from email.mime import image
 from tkinter import image_names
 from flask import Flask
 from flask import jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello() :
-    return jsonify({"Message":"Hello World !"})
+     return jsonify({"Message":os.environ.get('message')})
 
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=os.environ.get('Port'))
